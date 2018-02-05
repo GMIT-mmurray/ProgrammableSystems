841d9defe686e39beac0a375ddea831141d4214f
mac <michael.murray@gmit.ie>
2018-02-05 19:10:37 +0000

    update 5.2.18

---
+++ GroupB/CreateLInkedLIst.c
@@ -0,0 +1,61 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+
+struct node
+{
+	int data;
+	struct node *next;
+};
+
+struct node *start = NULL;
+struct node *display_ll(struct node *);
+
+int main(int argc, char *argv[]) 
+{
+	
+	struct node *new_node, *ptr;
+	int num;
+
+	printf(" \n Enter -1 to stop data entry ");
+	printf(" \n Enter the node's data: ");
+	scanf("%d", &num);
+
+	while(num != -1) {
+		new_node = (struct node*)malloc(sizeof(struct node));
+		new_node -> data = num;
+		if(start==NULL) {
+			new_node -> next = NULL;
+			start = new_node;
+		}
+		else {
+			ptr = start;
+			while(ptr->next != NULL) {
+				ptr = ptr->next;
+				} 
+				ptr->next = new_node;
+				new_node->next = NULL;
+			}
+		
+		printf(" \n Enter the data: ");
+		scanf("%d", &num);
+	}
+	
+	if(num == -1)
+		start = display_ll(start);
+
+	return 0;
+}
+
+struct node *display_ll(struct node *start) 
+{
+               struct node *ptr;
+               ptr = start;
+
+               while(ptr != NULL) {
+                       printf("\t %d", ptr -> data);
+			ptr = ptr -> next;
+		}
+
+		return start;
+}
